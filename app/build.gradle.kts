495393fb5de1b7b49a3d66037a97aa3f8d9891f3
Tung Nguyen <Tung.Nguyen@datalogic.com>
2026-01-09 11:33:10 +0700

    Update version 2.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "2.4.4"
+        versionName = "2.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
